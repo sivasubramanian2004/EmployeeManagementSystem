19383514fac6e74afa07dca474cb2ee1040e889a
sivasubramanian2004 <shivasubramanian3332@gmail.com>
2026-08-06 14:15:37 +0530

    repository changes

--- EMS.Data/Repositories/Repository.cs
+++ EMS.Data/Repositories/Repository.cs
@@ -43,7 +43,7 @@ public class Repository<T>(EmsDbContext context) : IRepository<T> where T : clas
         }
 
         /// <summary>
-        /// Gets an entity by its identifier asynchronously.
+       // /// Gets an entity by its identifier asynchronously.
         /// </summary>
         /// <param name="id">The identifier.</param>
         /// <returns>The entity.</returns>
